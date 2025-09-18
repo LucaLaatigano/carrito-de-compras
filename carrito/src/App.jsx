f82817c1da8d9cbcd0f3ec7ba790327f42905c19
Luca Santiago Latigano <lucalatigano12@gmail.com>
2025-09-18 02:42:14 -0300

    just learn how to pass context

--- carrito/src/App.jsx
+++ carrito/src/App.jsx
@@ -1,35 +1,49 @@
+import { useState, createContext } from "react";
 import "./App.css"
 import Carrito from "./Carrito";
+
+
+export const ThingsContext = createContext()
 function App(){
-
-
   const things = [
-    {name: "canarias", id:1, source:"./src/images/canarias.jpg"},
-    {name: "verdecita", id:2, source:"./images/canarias.jpg"},
-    {name: "sara", id:3, source:"./images/canarias.jpg"},
-    {name: "baldo", id:4, source:"./images/canarias.jpg"},
-    {name: "playadito", id:5, source:"./images/canarias.jpg"},
+    {name: "canarias", id:1, source:"fa"},
+    {name: "verdecita", id:2, source:"fa"},
+    {name: "sara", id:3, source:"fa"},
+    {name: "baldo", id:4, source:"fa"},
+    {name: "playadito", id:5, source:"fa"},
   ]
+
+  const [arrayThing, setArrayThing] = useState([])
+  const handleClick = (element)=>{
+    setArrayThing([...arrayThing, element])
+  }
+
 
 
   return(
     <div className="main">
       <div className="wrraper">
         <h2 className="title">Carrito de compras</h2>
-        <Carrito />
-
-        {things.map((thing)=>(
-              <div key={thing.id} className="mainwrraper">
-                  <img src={thing.source} alt="" className="image"/>
-                  <div className="name-wrraper">
-                    <h3 className="thing-name">
-                      {thing.name}
-                    </h3>
-                  </div>
+          <ThingsContext.Provider value={arrayThing}>
+            <Carrito />
+          </ThingsContext.Provider>
+        
+        <div className="things-container">
+          {things.map((thing) => (
+            <div key={thing.id} className="mainwrraper">
+              <img src={thing.source} alt={thing.name} className="image"/>
+              <div className="name-wrraper">
+                <h3 className="thing-name">
+                  {thing.name}
+                </h3>
+                <button onClick={()=> handleClick(thing)}>Add to cart</button>
               </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
 }
+
 export default App;
--- carrito/src/Carrito.jsx
+++ carrito/src/Carrito.jsx
@@ -1,11 +1,23 @@
 import { FaShoppingCart } from 'react-icons/fa';
+import { useContext, useState, useEffect } from 'react';
+import { ThingsContext } from "./App";
 import "./Carrito.css"
 function Carrito(){
+    const value = useContext(ThingsContext)
+    const [count, setCount] = useState(-1)
+
+    useEffect(()=>{
+    
+        setCount(count+1)
+        
+    }, [value])
     return(
         <div className="container" >
             <button >
                 <FaShoppingCart className="cart"/>
             </button>
+                {count > 0 && <span>{count}</span> }
+                
         </div>
     )
 
